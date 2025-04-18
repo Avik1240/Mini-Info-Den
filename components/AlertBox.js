c16587e0b6ca488c876d1dc2c868fc09d62e499b
Avik <avik.jain@silvertouch.com>
2025-04-18 12:30:41 +0530

    UI

--- components/AlertBox.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/AlertBox.module.css";
-
-export default function AlertBox({ message }) {
-  return (
-    <div className={styles.alert}>
-      <p>{message}</p>
-    </div>
-  );
-}
